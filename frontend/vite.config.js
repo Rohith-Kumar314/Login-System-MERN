b5ecce7c5aa85b40f5332ed9851063c46fff5545
Rohith-Kumar314 <23mcmc16@uohyd.ac.in>
2025-11-02 16:27:44 +0530

    Configure allowedHosts in Vite server settings
    Added allowedHosts configuration for the Vite server.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['login-system-mern-1-o7ju.onrender.com', 'localhost', '0.0.0.0'],
+    // You can also specify the host as 'all' to allow any host (but be cautious with this)
+    // allowedHosts: 'all',
+  },
 })
